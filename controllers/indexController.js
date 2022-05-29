2fada6460c57e313ecd52aed021710f4160ca6d4
vaniacaro07 <vaniacaballero5@gmail.com>
2022-05-29 16:46:50 -0300

    formulario

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,11 +5,11 @@ const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const smartphones = products.filter(product => product.category == "smartphone")
-const enOferta = products.filter(product => product.category == "en-oferta")
+const inSale = products.filter(product => product.category == "inSale")
 
 const indexController={
     index : (req, res) => {
-        res.render('products/index', {smartphones,enOferta});
+        res.render('products/index', {smartphones,inSale});
     },
 }
 module.exports = indexController;
