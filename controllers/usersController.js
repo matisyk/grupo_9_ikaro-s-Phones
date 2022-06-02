72aa04853017b68b426898278668efa74bb9be13
vaniacaro07 <vaniacaballero5@gmail.com>
2022-06-02 20:52:26 -0300

    product edit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,35 +1,31 @@
 const fs = require('fs');
 const path = require("path");
 
-const usersFilePath = path.join(__dirname, '../data/users.json');
-const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+// const usersFilePath = path.join(__dirname, '../data/users.json');
+// const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const usersController ={
     register:(req, res) => {
         res.render('register');
     },
-    saveUser: (req, res) =>{
-        let user = {
-            firstName: req.body.name,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: req.body.password,
-            confirm: req.body.confirm
-        }
-
-        let usuarios;
-        if(users == "") {
-            usuarios = [];
-        }
-    
-        usuarios.push(user);
-
-        let usuariosJSON = JSON.stringify(usuarios)
-
-        fs.writeFileSync(usersFilePath, usuariosJSON);
-	    
-        res.redirect('/index',)
-    },
+    // saveUser: (req, res) =>{
+    //     let user = {
+    //         firstName: req.body.name,
+    //         lastName: req.body.lastName,
+    //         email: req.body.email,
+    //         password: req.body.password,
+    //         confirm: req.body.confirm
+    //     }
+    //     let usuarios;
+    //     if(users == "") {
+    //         usuarios = [];
+    //     }else{
+    //         usuarios.push(user);
+    //     }
+    //     let usuariosJSON = JSON.stringify(usuarios)
+    //     fs.writeFileSync(usersFilePath, usuariosJSON);
+    //     res.redirect('/',)
+    // },
     login: (req, res) => {
         res.render('login');
     },
--- routers/users.js
+++ routers/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersController = require("../controllers/usersController")
 
 router.get('/register', usersController.register)
-router.post('/register', usersController.saveUser)
+// router.post('/register', usersController.saveUser)
 router.get('/login', usersController.login)
 
 module.exports = router
