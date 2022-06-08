4f1a398e9f40f01c142f4c6ca15ce981e56c367c
Fabi12902 <fabiansilva611@gmail.com>
2022-06-07 21:54:40 -0300

    crear producto con error en agregar imagen

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -21,14 +21,24 @@ const productController ={
         res.render('productCreate', {products})
     },
     created: (req, res, next) => {  //ruta POST guardar product
-        let file = req.file;
-        if(!file){
-            const error = new Error ('Por favor seleccione un archivo')
-            error.httpStatusCode = 400
-            return next (error)
-        }else{
-            res.redirect('index', {product})
+
+        let image 
+        if(req.files[0] != undefined){
+            image = req.files[0].filename
         }
+        else{
+            image = "default-image.png" 
+        }
+
+        let newProduct = {
+            id:products[products.length - 1].id + 1,
+            ...req.body,
+            image:image,
+        }
+        products.push(newProduct)
+        fs.writeFileSync(productsFilePath, JSON.stringify(products));
+        res.redirect("./products")
+
     },
     edit: (req, res) => {  //ruta GET editar el product
         res.render('productEdit', {products})
