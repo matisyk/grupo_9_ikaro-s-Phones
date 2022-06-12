1d2f7d5a89c12678d94a59b91785962ad01166a0
vaniacaro07 <vaniacaballero5@gmail.com>
2022-06-12 01:18:16 -0300

    users y validaciones

---
+++ middlewares/registerMiddleware.js
@@ -0,0 +1,12 @@
+const { body } = require('express-validator')
+
+const userValidate = [
+    body('fullName').notEmpty().withMessage('Debe completar este campo'),
+    body('email').notEmpty().withMessage('Debe completar este campo').bail()
+    .isEmail().withMessage('Debe ingresar una dirección de correo válido'),
+    body('password').notEmpty().withMessage('Debe completar este campo').bail()
+    .isLength({min:6, max:10}).withMessage('La contraseña debe tener entre 6-10 caracteres'),
+    body('confirm').notEmpty().withMessage('Debe completar este campo'),
+]
+
+module.exports = userValidate;
--- routers/users.js
+++ routers/users.js
@@ -1,21 +1,13 @@
 const express = require("express");
 const path = require('path');
 const router = express.Router();
-const { body } = require('express-validator')
+const registerValidate = require('../middlewares/registerMiddleware')
 
 const usersController = require("../controllers/usersController")
 
-const validate = [
-    body('fullName').notEmpty().withMessage('Debes completar este campo'),
-    body('email').notEmpty().withMessage('Debes completar este campo').bail()
-    .isEmail().withMessage('Debes escribir una dirección de correo válido'),
-    body('password').notEmpty().withMessage('Debes completar este campo'),
-    body('confirm').notEmpty().withMessage('Debes completar este campo'),
-]
-
 //formulario de registro
 router.get('/register', usersController.register)
-router.post('/register',  validate, usersController.saveUser)
+router.post('/register', registerValidate, usersController.saveUser)
 
 //formulario de login
 router.get('/login', usersController.login)
